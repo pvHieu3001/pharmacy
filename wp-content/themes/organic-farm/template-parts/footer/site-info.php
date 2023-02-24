f8313ce1d143c950c1678ce6e8de0af8265b9b35
pham hieu <phamvanhieu300198@gmail.com>
2023-02-25 00:37:32 +0700

    ww

--- wp-content/themes/organic-farm/template-parts/footer/site-info.php
+++ wp-content/themes/organic-farm/template-parts/footer/site-info.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 /**
  * Displays footer site info
  *
@@ -8,13 +8,13 @@
  */
 
 ?>
-// <div class="site-info py-4 text-center">
-// 	<?php
-// 		echo esc_html( get_theme_mod( 'organic_farm_footer_text' ) );
-// 		printf(
-// 			/* translators: %s: Organic Farm WordPress Theme. */
-// 			esc_html__( ' %s ', 'organic-farm' ),
-// 			'<a target="_blank" href="' . esc_url( 'https://www.ovationthemes.com/wordpress/free-organic-wordpress-theme/') . '"></a>'
-// 		);
-// 	?>
-// </div>
+ <div class="site-info py-4 text-center">
+ 	<?php
+ 		echo esc_html( get_theme_mod( 'organic_farm_footer_text' ) );
+ 		printf(
+ 			/* translators: %s: Organic Farm WordPress Theme. */
+ 			esc_html__( ' %s ', 'organic-farm' ),
+ 			'<a target="_blank" href="' . esc_url( 'https://www.ovationthemes.com/wordpress/free-organic-wordpress-theme/') . '"></a>'
+		);
+ 	?>
+ </div> -->
