e15c8b150fb9f5e596c6cd650f067adc55c728ff
root <root@instance-test.asia-east1-b.c.sonic-arcadia-377714.internal>
2023-03-08 15:50:21 +0000

    haha

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp' );
+define( 'DB_NAME', 'beautysalon' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
